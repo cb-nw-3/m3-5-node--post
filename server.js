0366c1f9e47cce48e9b363b629631945eb70e03a
donnyesq <adonis@darmas.ca>
2020-06-13 16:34:08 -0400

    exercise 1 complete

--- server.js
+++ server.js
@@ -1,27 +1,36 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 8000;
 
+const { stock, customers } = require("./data/promo");
+const items = [];
+
 express()
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('tiny'))
-  .use(express.static('public'))
+  .use(morgan("tiny"))
+  .use(express.static("public"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
-  .set('view engine', 'ejs')
+  .set("view engine", "ejs")
 
   // endpoints
+  .get("/todos", (req, res) => res.render("../views/todos", { items }))
 
-  .get('*', (req, res) => res.send('Dang. 404.'))
+  .post("/data", (req, res) => {
+    items.push(req.body.item);
+    res.redirect("/todos");
+  })
+
+  .get("*", (req, res) => res.send("Dang. 404."))
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
