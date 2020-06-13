e1342e44258ae64e1063814f29621b5820b93779
rogerlam <rogerlam@wylco.org>
2020-06-13 13:17:00 -0400

    finish ex 1

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ const morgan = require('morgan');
 
 const PORT = process.env.PORT || 8000;
 
+const data = [];
+
+const handleHomepage = (req, res) => {
+  res.render('homepage', { data: data });
+}
+
+const addItem = (req, res) => {
+
+  // pull new item from the request body
+  // append to the existing list of items
+
+  let newItem = req.body.newitem;
+  data.push(newItem);
+  res.redirect('/');
+}
+
 express()
   .use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -22,6 +38,10 @@ express()
   .set('view engine', 'ejs')
 
   // endpoints
+
+  .get('/todos', handleHomepage)
+
+  .post('/data', addItem)
 
   .get('*', (req, res) => res.send('Dang. 404.'))
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
