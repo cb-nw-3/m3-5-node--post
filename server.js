554c3098cdf478fb77108030055477430d437782
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-06-13 13:30:19 -0400

    ex1 created endpoint and todo ejs file

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const { handleHomePage } = require("./routes/routes.js");
 
 const PORT = process.env.PORT || 8000;
 
 express()
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('tiny'))
-  .use(express.static('public'))
+  .use(morgan("tiny"))
+  .use(express.static("public"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
-  .set('view engine', 'ejs')
+  .set("view engine", "ejs")
 
   // endpoints
+  .get("/todos", handleHomePage)
 
-  .get('*', (req, res) => res.send('Dang. 404.'))
+  .get("*", (req, res) => res.send("Dang. 404."))
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
