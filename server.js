454356d35c98dac976b97d337aa969bc74f5d34e
Etienne Giroux <etienneg@Etiennes-MacBook-Pro.local>
2020-06-13 11:22:32 -0400

    Functional version of todo list (boring)

--- server.js
+++ server.js
@@ -1,27 +1,38 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 8000;
 
+let items = [];
+
 express()
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('tiny'))
-  .use(express.static('public'))
+  .use(morgan("tiny"))
+  .use(express.static("public"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
-  .set('view engine', 'ejs')
+  .set("view engine", "ejs")
 
   // endpoints
 
-  .get('*', (req, res) => res.send('Dang. 404.'))
+  .get("/todos", (req, res) => res.render("./pages/todos", { items: items }))
+  .post("/data", (req, res) => {
+    const { item } = req.body;
+
+    items.push(item);
+
+    res.redirect("/todos");
+  })
+
+  .get("*", (req, res) => res.send("Dang. 404."))
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
