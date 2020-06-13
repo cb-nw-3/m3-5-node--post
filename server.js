bb00c6251d2e4ad4ae131ab10e9c9cdd86ef33ae
garorueda99 <59367534+garorueda99@users.noreply.github.com>
2020-06-13 11:38:44 -0400

    post receiving info

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 8000;
 
+const handleToDos = (req, res) => {
+  res.status(200).render("pages/todos");
+};
+
+const handleData = (req, res) => {
+  console.log(req.body);
+};
+
 express()
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('tiny'))
-  .use(express.static('public'))
-  .use(bodyParser.json())
+  .use(morgan("tiny"))
+  .use(express.static("public"))
+  .use(express.json())
   .use(express.urlencoded({ extended: false }))
-  .set('view engine', 'ejs')
+  .set("view engine", "ejs")
 
   // endpoints
 
-  .get('*', (req, res) => res.send('Dang. 404.'))
+  .get("/todos", handleToDos)
+  .post("/data", handleData)
+  .get("*", (req, res) => res.send("Dang. 404."))
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
