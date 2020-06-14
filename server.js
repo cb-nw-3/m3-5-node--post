7ddaffec2c096bc8cc0b8bc5e29762593de5184f
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-06-14 15:57:26 -0400

    attempt #3, fail

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const { handleToDos, handleData} = require('./handlers');
 const { stock, customers} = require('./data/promo');
+const { response } = require("express");
 
 const PORT = process.env.PORT || 8000;
 
@@ -27,8 +28,20 @@ express()
   .get("/", handleToDos)
   .post('/data', handleData)
   .post('/order', (req,res) => {
-    console.log(req.body);
-    res.render('../order-confirmed', { data: req.body});
-  })
+    if (error === 'unavailable') {
+      res.send({ status: 'error', error: unavailable});
+    }
+    else if (error === 'repeat-customer') {
+      res.send({ status: 'error', error: 'repeat-customer'})
+    }
+    else if (error === 'undeliverable') {
+      res.send({ status: 'error', error: undeliverable})
+    }
+    if (error === 'missing-data') {
+      res.send({ status: 'error', error: 'missing-data'})
+    }
+    res.send({status: 'success'});
+
+    })
   .get("*", (req, res) => res.send("Dang. 404."))
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
