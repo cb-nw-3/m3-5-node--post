125a36cbd84cf6ee965ceb6a77c1dbb14012b002
jenny ma <jenny_lau_8@hotmail.com>
2020-06-15 18:00:49 -0400

    fetch promo

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const morgan = require('morgan');
 //empty array for the todo list
 const items = [];
 
+const { stock, customers } = require('./data/promo.js');
+
 const PORT = process.env.PORT || 8000;
 
 express()
@@ -26,12 +28,18 @@ express()
 
 
   // endpoints
+  // endpoints workshop #1
   .get('/todos', (req, res) => res.render('./pages/todos', { items: items}))
   .post('/data', (req, res) => {
     const { item } = req.body;
     items.push(item);
     res.redirect('/todos');
   })
+
+  //endpoints workshop #2
+  .post('/order', (req, res) => { 
+    stock: stock
+    customers: customers});
 
   .get('*', (req, res) => res.send('Dang. 404.'))
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
