85967f2d30024c573958145f05f1410128d35fc6
kolbymckeown <mckeownkolby@gmail.com>
2020-06-15 21:44:42 -0400

    Order form started

--- server.js
+++ server.js
@@ -1,43 +1,95 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const { stock, customers } = require("./data/promo");
 
 const PORT = process.env.PORT || 8000;
 
+// todo list items
 const items = [];
 
 const handleFormData = (req, res) => {
-  const { item } = req.body;
-  items.push(item);
-  res.render('pages/homepage', { items: items })
-
-}
+	const { item } = req.body;
+	items.push(item);
+	res.render("pages/homepage", { items: items });
+};
 
 express()
-  .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-  })
-  .use(morgan('tiny'))
-  .use(express.static('public'))
-  .use(bodyParser.json())
-  .use(express.urlencoded({ extended: false }))
-  .set('view engine', 'ejs')
+	.use(function (req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header(
+			"Access-Control-Allow-Headers",
+			"Origin, X-Requested-With, Content-Type, Accept"
+		);
+		next();
+	})
+	.use(morgan("tiny"))
+	.use(express.static("public"))
+	.use(bodyParser.json())
+	.use(express.urlencoded({ extended: false }))
+	.set("view engine", "ejs")
 
-  // endpoints
-  // todo endpoints
-.get('/todos', (req, res) => {
-  res.render('pages/homepage', { items: items })
-})
-.post('/data', handleFormData)
+	// endpoints
+	// todo endpoints
+	.get("/todos", (req, res) => {
+		res.render("pages/homepage", { items: items });
+	})
+	.post("/data", handleFormData)
 
-  // order form endpoints
+	// order form endpoints
+	.get("/order-form", (req, res) => {
+		res.render("pages/orders");
+	})
 
-  .get('*', (req, res) => res.send('Dang. 404.'))
-  .listen(PORT, () => console.log(`Listening on port ${PORT}`));
+	.post("/order", (req, res) => {
+		console.log(req.body);
+		const { givenName } = req.body;
+		const { surname } = req.body;
+		const { country } = req.body;
+
+		// make if statements to check if there are errors
+		// import  promo.js to have acceess to stocks and customers
+		// compare req.body information to stocks and customer to see if there are errors
+		if (
+			customers.find(
+				(customer) =>
+					customer.givenName === givenName && surname === customer.surname
+			)
+		) {
+			res.send(
+				JSON.stringify({
+					status: "error",
+					error: "repeat-customer",
+				})
+			);
+		} else if (stock === "0") {
+			res.send(
+				JSON.stringify({
+					status: "error",
+					error: "Item out of stock. :(",
+				})
+			);
+		} else if (customers.find((customer) => customer.country !== "Canada")) {
+			res.send(
+				JSON.stringify({
+					status: "error",
+					error: "Outside of delivery zone. :(",
+				})
+			);
+		}
+		// Orders confirming when they shouldn't be ^^^ (stock, country)
+		res.send(
+			JSON.stringify({
+				status: "success",
+			})
+		);
+	})
+
+	.get("/order-confirmed", (req, res) => {
+		res.render("pages/order-confirmed");
+	})
+
+	.get("*", (req, res) => res.send("Dang. 404."))
+	.listen(PORT, () => console.log(`Listening on port ${PORT}`));
