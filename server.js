b08ff71716ab55ff95ae3d12aa21a4d384a214a6
kolbymckeown <mckeownkolby@gmail.com>
2020-06-15 20:33:07 -0400

    Exercise 1 finished except styles

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const morgan = require('morgan');
 
 const PORT = process.env.PORT || 8000;
 
+const items = [];
+
+const handleFormData = (req, res) => {
+  const { item } = req.body;
+  items.push(item);
+  res.render('pages/homepage', { items: items })
+
+}
+
 express()
   .use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -22,6 +31,13 @@ express()
   .set('view engine', 'ejs')
 
   // endpoints
+  // todo endpoints
+.get('/todos', (req, res) => {
+  res.render('pages/homepage', { items: items })
+})
+.post('/data', handleFormData)
+
+  // order form endpoints
 
   .get('*', (req, res) => res.send('Dang. 404.'))
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
