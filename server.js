951342b15828e1a3319b167f6aea3054177f4369
Truong Dao <truong.dao.do@gmail.com>
2020-06-15 20:52:12 -0400

    Solutions_Node5_TD

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const { handleHomePage, handleFormData, handleForm } = require('./handlers');
+// handleHomePage and HandleFormData for Exercice 1
+// handleForm for Exercice 2
 
 const PORT = process.env.PORT || 8000;
 
@@ -22,6 +25,15 @@ express()
   .set('view engine', 'ejs')
 
   // endpoints
+  .get('/todos', handleHomePage)
+  .post('/form-data', handleFormData)
+  // Added for Exercice 1
+
+  .post('/order', handleForm)
+  // Added for Exercice 2
 
   .get('*', (req, res) => res.send('Dang. 404.'))
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+// For this Exercice 2 we need to create a folder "order-confirmed" and file "index.html" within the newly created folder.
+// The page will be redirected automaticaly because of line 20 in this file.
