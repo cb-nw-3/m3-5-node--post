94b797b3456da8e1f11039d37ab93486d7f26e18
Etienne Giroux <etienneg@Etiennes-MacBook-Pro.local>
2020-06-15 20:26:20 -0400

    added done functionality to list

--- server.js
+++ server.js
@@ -49,16 +49,28 @@ express()
 
   .post("/data", (req, res) => {
     const { item } = req.body;
-    items.push(item);
+    let listItem = {
+      name: item,
+    };
+
+    items.push(listItem);
     res.redirect("/todos");
   })
 
   // I know i'm supposed to use .delete but the same code triggers a 404 when the method is delete (both in the html and here)
+  .post("/doneItem", (req, res) => {
+    const { itemName } = req.body;
+    let thisItem = items.find((item) => item.name === itemName);
+    thisItem.done = true;
+    res.redirect("/todos");
+  })
+
   .post("/deleteItem", (req, res) => {
     const { index } = req.body;
     items.splice(index, 1);
     res.redirect("/todos");
   })
+
   .post("/order", handleOrderSubmit)
 
   .get("*", (req, res) => res.send("Dang. 404."))
