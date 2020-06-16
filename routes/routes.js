1057705f32c3086afe9023c50c24251e28863afc
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-06-15 20:26:21 -0400

    ex2 added stock validation and comments

--- routes/routes.js
+++ routes/routes.js
@@ -23,15 +23,98 @@ const handleData = (req, res) => {
 };
 
 const handleForm = (req, res) => {
+  //obtain the response data from the form
   console.log(req.body);
   let completedForm = req.body;
 
-  let customerNames = [];
-  let customerAddress = [];
-  customers.forEach((customer) => customerNames.push(customer.givenName));
-  customers.forEach((customer) => customerAddress.push(customer.address));
-  console.log(customerNames);
+  //this will handle stock validation for tshirts in various sizes
+  if (completedForm.order === "shirt") {
+    //let { small, medium, large, xlarge } = stock.shirt;
+    //console.log(small, medium, large, xlarge);
 
+    //depending on what size, we check if its in stock, if not decrement
+    //and send back json response
+    switch (completedForm.size) {
+      case "small":
+        if (stock.shirt.small > 0) {
+          console.log("you bought a small shirt");
+          stock.shirt.small--;
+          break;
+        } else {
+          res.send(JSON.stringify({ status: "error", error: "unavailable" }));
+          break;
+        }
+      case "medium":
+        if (stock.shirt.medium > 0) {
+          console.log("you bought a medium shirt");
+          stock.shirt.medium--;
+          break;
+        } else {
+          console.log("no more shirts in this size");
+          res.send(JSON.stringify({ status: "error", error: "unavailable" }));
+          break;
+        }
+      case "large":
+        if (stock.shirt.large > 0) {
+          console.log("you bought a large shirt");
+          stock.shirt.large--;
+          break;
+        } else {
+          console.log("no more shirts in this size");
+          res.send(JSON.stringify({ status: "error", error: "unavailable" }));
+          break;
+        }
+      case "xlarge":
+        if (stock.shirt.xlarge > 0) {
+          console.log("you bought a xlarge shirt");
+          stock.shirt.xlarge--;
+          break;
+        } else {
+          console.log("no more shirts in this size");
+          res.send(JSON.stringify({ status: "error", error: "unavailable" }));
+          break;
+        }
+    }
+    //get a total tally of current stock to see if decrement works
+    //console.log("you now have", stock.shirt);
+  }
+
+  //this will cover the bottles and socks stock validation
+  switch (completedForm.order) {
+    case "bottles":
+      if (stock.bottles > 0) {
+        completedForm.size = 1;
+        req.body.size = 1;
+        stock.bottles--;
+        console.log("your stock in socks is now", stock.bottles);
+        break;
+      } else {
+        console.log("no more bottles left");
+        res.send(JSON.stringify({ status: "error", error: "unavailable" }));
+        break;
+      }
+    case "socks":
+      if (stock.socks > 0) {
+        completedForm.size = 1;
+        req.body.size = 1;
+        stock.socks--;
+        console.log("your stock in socks is now", stock.socks);
+        break;
+      } else {
+        console.log("no more socks left");
+        res.send(JSON.stringify({ status: "error", error: "unavailable" }));
+        break;
+      }
+  }
+
+  //let customerNames = [];
+  //let customerAddress = [];
+  //customers.forEach((customer) => customerNames.push(customer.givenName));
+  //customers.forEach((customer) => customerAddress.push(customer.address));
+  //console.log(customerNames);
+
+  //loop through each customer object and verify that their name and address
+  //are not repeated on the form
   customers.forEach((customer) => {
     if (
       customer.givenName === completedForm.givenName &&
@@ -42,6 +125,7 @@ const handleForm = (req, res) => {
     }
   });
 
+  //verify that the country the product is shipping to is Canada
   if (completedForm.country !== "Canada") {
     console.log("Cannot deliver outside Canada");
     res.send(JSON.stringify({ status: "error", error: "undeliverable" }));
@@ -52,12 +136,16 @@ const handleForm = (req, res) => {
   //   res.send(JSON.stringify({ status: "error", error: "undeliverable" }));
   // }
 
+  //if all fields in the form are not undefined then form has been filled
+  //properly
   if (!Object.values(completedForm).includes("undefined")) {
     console.log("Form is Completed!");
     res.send(JSON.stringify({ status: "success" }));
+    console.log(completedForm);
   } else {
     console.log("form is missing info");
     res.send(JSON.stringify({ status: "error", error: "missing-data" }));
+    console.log(completedForm);
   }
 
   // if (
@@ -72,9 +160,17 @@ const handleForm = (req, res) => {
   // }
 };
 
+//this is part of the stretch goal to render dynamic info in the confirmation page
+const handleOrder = (req, res) => {
+  let completedForm = req.body;
+  console.log("handling the order now for: ", completedForm);
+  res.render("order-confirmed", { data: completedForm });
+};
+
 //export all handlers
 module.exports = {
   handleHomePage: handleHomePage,
   handleData: handleData,
   handleForm: handleForm,
+  handleOrder: handleOrder,
 };
