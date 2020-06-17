7b3f946433e71660810e17f625a2e12926342c9d
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-06-17 12:42:14 -0400

    comments

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict';
-
+// to do list elements
 const toDoList = [];
-
+// status object to modify into json and send it
 let orderStatus = {};
-
+// order/customer information
 let order;
-
+// customer object to push it into array data
 let newCustomerObject = {};
 
 const { stock, customers } = require('./data/promo.js');
@@ -28,9 +28,10 @@ const handleDataToDoList = (req, res) => {
 }
 
 const handleOrder = (req, res) => {
+  // variable to check if customer is new or not
   let newCustomer = true;
+  // form data is inserted in this variable
   order = req.body;
-
   // check if good order
   customers.forEach(element => {
     // new customer
@@ -52,53 +53,78 @@ const handleOrder = (req, res) => {
   });
 
   // data object to send depending on survey data
+  // if not new customer
   if (!newCustomer) {
+    // status change
     orderStatus = {
       status: "error",
       error: "repeat-customer"
     }
     res.send(JSON.stringify(orderStatus))
+  // if not in canada
   } else if (req.body.country !== 'Canada' && req.body.country !== 'canada') {
+    // status change
     orderStatus = {
       status: "error",
       error: "undeliverable"
     }
+    // status send
     res.send(JSON.stringify(orderStatus))
+  // if order is a shirt
   } else if (req.body.order === 'shirt') {
+    // if item is not in stock
     if (stock[order.order][order.size] <= 0) {
+      // status change
       orderStatus = {
         status: "error",
         error: "unavailable"
       }
+      // status send
       res.send(JSON.stringify(orderStatus))
+    // if item is in stock
     } else {
       stock[order.order][order.size]--;
+      // status change
       orderStatus = {
         status: 'success'
-      } 
+      }
+      // add customer tocustomerrs array
       customers.push(newCustomerObject)
+      // status send
       res.send(JSON.stringify(orderStatus))
     }
+  // if item is socks or bottles
   } else if (req.body.order === 'socks' || req.body.order === 'bottles') {
+    // if item is not in stock
     if (stock[order.order] <= 0) {
+      // status change
       orderStatus = {
         status: "error",
         error: "unavailable"
       }
+      // status send
       res.send(JSON.stringify(orderStatus))
+    // if item is in stock
     } else {
       stock[order.order]--;
+      // status change
       orderStatus = {
         status: 'success'
-      } 
+      }
+      // add customer tocustomerrs array
       customers.push(newCustomerObject)
+      // status send
       res.send(JSON.stringify(orderStatus))
     }
+  // if everything is good
   } else {
+    // status change
     orderStatus = {
       status: 'success'
     }
+    // add customer tocustomerrs array
     customers.push(newCustomerObject)
+    // status send
     res.send(JSON.stringify(orderStatus));
   }
   res.redirect('/order-confirmed')
