b2f3db23ce98fa593723081dfecb8b8f78f788f6
donnyesq <adonis@darmas.ca>
2020-06-17 14:20:39 -0400

    all checks work except missing-data

--- server.js
+++ server.js
@@ -7,7 +7,84 @@ const morgan = require("morgan");
 const PORT = process.env.PORT || 8000;
 
 const { stock, customers } = require("./data/promo");
+
 const items = [];
+
+const findCustomer = (name, surname, address) => {
+  let nameCheck;
+  let surnameCheck;
+  let addressCheck;
+
+  customers.forEach((customer) => {
+    if (customer.givenName === name) {
+      nameCheck = true;
+    }
+  });
+
+  customers.forEach((customer) => {
+    if (customer.surname === surname) {
+      surnameCheck = true;
+    }
+  });
+
+  customers.forEach((customer) => {
+    if (customer.address === address) {
+      addressCheck = true;
+    }
+  });
+
+  return nameCheck && surnameCheck && addressCheck;
+};
+
+const isOverseas = (country) => {
+  let overseasCustomer = false;
+
+  customers.forEach((customer) => {
+    if (customer.country !== country) {
+      overseasCustomer = true;
+    }
+  });
+  return overseasCustomer;
+};
+
+const isOutOfStock = (item, size) => {
+  let itemIsOutOfStock = false;
+
+  if (item === "shirt") {
+    if (Number(stock[item][size]) < 1) {
+      itemIsOutOfStock = true;
+    } else {
+      if (Number(stock[item]) < 1) {
+        itemIsOutOfStock = true;
+      }
+    }
+  } else {
+    if (stock[item] < 1) {
+      itemIsOutOfStock = true;
+    }
+  }
+
+  return itemIsOutOfStock;
+};
+
+const isDataMissing = (data) => {
+  let dataIsMissing;
+
+  if (
+    data.givenName === "" ||
+    data.surname === "" ||
+    data.address === "" ||
+    data.country === "" ||
+    data.order === "" ||
+    data.size === "" ||
+    data.email === "" ||
+    data.city === "" ||
+    data.province === "" ||
+    data.postcode === ""
+  ) {
+    dataIsMissing = true;
+  }
+};
 
 express()
   .use(function (req, res, next) {
@@ -30,6 +107,57 @@ express()
   .post("/data", (req, res) => {
     items.push(req.body.item);
     res.redirect("/todos");
+  })
+
+  .post("/order", (req, res) => {
+    let response = {};
+
+    const existingCustomer = findCustomer(
+      req.body.givenName,
+      req.body.surname,
+      req.body.address
+    );
+
+    const overseas = isOverseas(req.body.country);
+
+    const outOfStock = isOutOfStock(req.body.order, req.body.size);
+
+    const missingData = isDataMissing(req.body);
+
+    if (existingCustomer) {
+      response = {
+        status: "error",
+        error: "repeat-customer",
+      };
+      res.json(response);
+    }
+
+    if (overseas) {
+      response = {
+        status: "error",
+        error: "undeliverable",
+      };
+      res.json(response);
+    }
+
+    if (outOfStock) {
+      response = {
+        status: "error",
+        error: "unavailable",
+      };
+      res.json(response);
+    }
+
+    if (missingData) {
+      response = {
+        status: "error",
+        error: "missing-data",
+      };
+      res.json(response);
+    }
+
+    response = { status: "success" };
+    res.json(response);
   })
 
   .get("*", (req, res) => res.send("Dang. 404."))
