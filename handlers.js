76b37d50880ed9267d5094d38076621b724e5a4e
garorueda99 <59367534+garorueda99@users.noreply.github.com>
2020-06-17 18:08:14 -0400

    refresh from parent

--- handlers.js
+++ handlers.js
@@ -1,44 +1,44 @@
-"use strict";
+'use strict';
 
-const { stock, customers } = require("./data/promo.js");
-const { checklist } = require("./data/checklist.js");
-const nodemon = require("nodemon");
+const { stock, customers } = require('./data/promo.js');
+const { checklist } = require('./data/checklist.js');
+const nodemon = require('nodemon');
 // const { resolveInclude } = require("ejs");
 const response = {
-  status: "success",
+  status: 'success',
 };
 
 let itemsList = checklist[0].list;
 
 //Handlers Exercise #1
 const handleToDos = (req, res) => {
-  res.status(200).render("pages/todos", { itemsList });
+  res.status(200).render('pages/todos', { itemsList });
 };
 
 const handleData = (req, res) => {
   const item = { text: req.body.item, checked: false };
   itemsList.push(item);
-  res.redirect("/todos");
+  res.redirect('/todos');
 };
 
 const handleUpdate = (req, res) => {
   itemsList = req.body;
   console.log(itemsList);
   // res.status(200).render("pages/todos", { itemsList });
-  res.redirect(301, "/todos");
+  res.redirect(301, '/todos');
 };
 
 //Handlers Exercise #2
 
 const handleOrder = (req, res) => {
-  response.status = "success";
+  response.status = 'success';
   delete response.error;
   const form = req.body;
   const formOk = formValidation(form);
   if (formOk) {
     orderValidation(form);
   }
-  if (response.status === "success") {
+  if (response.status === 'success') {
     const newCustomer = {
       givenName: form.givenName,
       surname: form.surname,
@@ -50,7 +50,7 @@ const handleOrder = (req, res) => {
       country: form.country,
     };
     customers.push(newCustomer);
-    form.size == "undefined"
+    form.size == 'undefined'
       ? (stock[form.order] = (parseInt(stock[form.order]) - 1).toString())
       : (stock.shirt[form.size] = (
           parseInt(stock.shirt[form.size]) - 1
@@ -63,9 +63,9 @@ function orderValidation(form) {
   let status = true;
 
   // Validates country as CANADA
-  if (form.country.toUpperCase() != "CANADA") {
-    response.status = "error";
-    response.error = "undeliverable";
+  if (form.country.toUpperCase() != 'CANADA') {
+    response.status = 'error';
+    response.error = 'undeliverable';
   }
   // Validates that the user has not yet placed an order
   firstTimeValidation(form);
@@ -79,15 +79,15 @@ function orderValidation(form) {
 function formValidation(form) {
   console.log(form);
   const status = true;
-  if (form.order == "undefined") {
-    response.status = "error";
-    response.error = "missing-data";
+  if (form.order == 'undefined') {
+    response.status = 'error';
+    response.error = 'missing-data';
     return false;
   } else {
     for (let key in form) {
-      if (form[key] == "") {
-        response.status = "error";
-        response.error = "missing-data";
+      if (form[key] == '') {
+        response.status = 'error';
+        response.error = 'missing-data';
         return false;
       }
     }
@@ -96,14 +96,14 @@ function formValidation(form) {
 }
 
 function stockValidation(form) {
-  if (form.size == "undefined") {
+  if (form.size == 'undefined') {
     if (parseInt(stock[form.order]) <= 0) {
-      response.status = "error";
-      response.error = "unavailable";
+      response.status = 'error';
+      response.error = 'unavailable';
     }
   } else if (parseInt(stock[form.order][form.size]) <= 0) {
-    response.status = "error";
-    response.error = "unavailable";
+    response.status = 'error';
+    response.error = 'unavailable';
   }
 }
 
@@ -113,8 +113,8 @@ function firstTimeValidation(form) {
       customer.givenName.toUpperCase() === form.givenName.toUpperCase() &&
       customer.surname.toUpperCase() === form.surname.toUpperCase()
     ) {
-      response.status = "error";
-      response.error = "repeat-customer";
+      response.status = 'error';
+      response.error = 'repeat-customer';
     }
   });
 }
@@ -127,8 +127,8 @@ function addressValidation(form) {
       customer.city.toUpperCase() === form.city.toUpperCase() &&
       customer.postcode.toUpperCase() === form.postcode.toUpperCase()
     ) {
-      response.status = "error";
-      response.error = "repeat-customer";
+      response.status = 'error';
+      response.error = 'repeat-customer';
     }
   });
 }
