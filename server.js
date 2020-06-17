25b47209b722d29a7c6ba92db4a29b83810652f1
jenny ma <jenny_lau_8@hotmail.com>
2020-06-16 22:32:18 -0400

    #2 complete

--- server.js
+++ server.js
@@ -25,13 +25,80 @@ const handleForm = (req, res) => {
       customer.givenName === completedForm.givenName &&
       customer.address === completedForm.address
     ) {
-      console.log("You have already purchased an item");
+     //Customer has already purchased an item
       res.send(JSON.stringify({ status: "error", error: "repeat-customer" }));
     }
   });
 
+  //tshirt inventory
+  if (completedForm.order === "shirt") {
+    //let { small, medium, large, xlarge } = stock.shirt;
+
+    //depending on what size, we check if its in stock, if not decrement
+    //and send back json response
+    switch (completedForm.size) {
+      case "small":
+        if (stock.shirt.small > 0) {
+          stock.shirt.small--;
+          break;
+        } else {
+          res.send(JSON.stringify({ status: "error", error: "unavailable" }));
+          break;
+        }
+      case "medium":
+        if (stock.shirt.medium > 0) {
+          stock.shirt.medium--;
+          break;
+        } else {
+          res.send(JSON.stringify({ status: "error", error: "unavailable" }));
+          break;
+        }
+      case "large":
+        if (stock.shirt.large > 0) {
+          stock.shirt.large--;
+          break;
+        } else {
+          res.send(JSON.stringify({ status: "error", error: "unavailable" }));
+          break;
+        }
+      case "xlarge":
+        if (stock.shirt.xlarge > 0) {
+          stock.shirt.xlarge--;
+          break;
+        } else {
+          res.send(JSON.stringify({ status: "error", error: "unavailable" }));
+          break;
+        }
+    }
+   
+  }
+
+  //bottles and socks inventory
+  switch (completedForm.order) {
+    case "bottle":
+      if (stock.bottles > 0) {
+        completedForm.size = 1;
+        stock.bottles--;
+        break;
+      } else {
+        //no more bottles left
+        res.send(JSON.stringify({ status: "error", error: "unavailable" }));
+        break;
+      }
+    case "socks":
+      if (stock.socks > 0) {
+        completedForm.size = 1;
+        stock.socks--;
+        break;
+      } else {
+        //no more socks left
+        res.send(JSON.stringify({ status: "error", error: "unavailable" }));
+        break;
+      }
+  }
+
   if (completedForm.country !== "Canada") {
-    console.log("Delivery is within Canada only");
+    //Delivery is within Canada only"
     res.send(JSON.stringify({ status: "error", error: "undeliverable" }));
   }
 
@@ -43,6 +110,7 @@ const handleForm = (req, res) => {
     res.send(JSON.stringify({ status: "error", error: "missing-data" }));
   }
 };
+console.log(stock.shirt.medium);
 
 //express
 express()
