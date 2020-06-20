0b58526162ff17aab69c39e1b2848679dba14e74
anwesh-sarkar <anwesh.sarkar@gmail.com>
2020-06-20 19:05:14 -0400

    Finished Exercise 1

--- server.js
+++ server.js
@@ -1,27 +1,45 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 8000;
 
+// store all the todo items in an array
+let item = [];
+
+const handleToDo = (req, res) => {
+  res.status(200).render("../pages/todo", {
+    item: item,
+  });
+};
+
+const handleAddItem = (req, res) => {
+  let newItem = req.body.item;
+  console.log(newItem);
+  item.push(newItem);
+  res.redirect("/todos");
+};
+
 express()
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('tiny'))
-  .use(express.static('public'))
+  .use(morgan("tiny"))
+  .use(express.static("public"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
-  .set('view engine', 'ejs')
+  .set("view engine", "ejs")
 
   // endpoints
+  .get("/todos", handleToDo)
+  .post("/addItem", handleAddItem)
 
-  .get('*', (req, res) => res.send('Dang. 404.'))
+  .get("*", (req, res) => res.send("Dang. 404."))
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
