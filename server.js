25955f2e70d07305e754778d5d7c3d305688f6e7
Alex Langevin <alex_langevin@hotmail.ca>
2020-06-27 21:43:40 -0400

    2 /4 validations working :)

--- server.js
+++ server.js
@@ -5,6 +5,31 @@ const taskList = [];
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const { stock, customers } = require('./data/promo');
+
+//Couple of functions for the validation
+function sameName (order){
+  let repeat = false;
+  const sur = order.surname;
+  const giv = order.givenName;
+  customers.forEach( cus => {
+    if(sur.toLowerCase() === cus.surname.toLowerCase() &&
+      giv.toLowerCase() === cus.givenName.toLowerCase()){
+      repeat = true;}
+  })
+  return repeat;
+}
+
+function sameStreet(order){
+  let repeat =  false;
+  const add = order.address;
+  customers.forEach( cus => {
+    if(add === cus.address){
+      repeat = true;
+    }
+  })
+  return repeat;
+}
 
 const PORT = process.env.PORT || 8000;
 
@@ -17,6 +42,30 @@ const handleTask = (req,res) => {
   const {task} = req.body;
   taskList.push(task);
   res.redirect('/todos');
+}
+
+const handleOrderConfirmed = (req, res) => {
+  //res.render not working with plain html??? Need to google this...
+  res.sendfile('public/order-confirmed.html');
+}
+
+const handleOrder = (req, res) => {
+  const order = req.body;
+  let orderInfo = {};
+
+  if(sameName(order)){ //Repeat customer-name
+    orderInfo.status = 'error';
+    orderInfo.error = 'repeat-customer';
+  } else if (sameStreet(order)){ //Repeat street-name
+    orderInfo.status = 'error';
+    orderInfo.error = 'repeat-customer';
+  } else if (order.country.toLowerCase() !== 'canada'){ //CANADA?
+    orderInfo.status = 'error';
+    orderInfo.error = 'undeliverable';
+  } else {
+    orderInfo.status = 'success';
+  }
+  res.json(orderInfo);
 }
 
 express()
@@ -37,6 +86,8 @@ express()
   // endpoints
   .get('/todos', handleToDoPage)
   .post('/task', handleTask)
+  .get('/order-confirmed', handleOrderConfirmed)
+  .post('/order', handleOrder)
 
   .get('*', (req, res) => res.send('Dang. 404.'))
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
